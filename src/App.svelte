135a41a4a932fd4a9652cdf214ef348206a7a4c0
Denis Wells <deniswells59@gmail.com>
2022-07-06 16:34:39 -0700

    rollin dice 🎲

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,37 @@
 <script lang="ts">
   import './app.css';
+
+  const getRandomByMinMax = (min, max) => {
+    const randomBuffer = new Uint32Array(1);
+
+    window.crypto.getRandomValues(randomBuffer);
+
+    let randomNumber = randomBuffer[0] / (0xffffffff + 1);
+
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(randomNumber * (max - min + 1)) + min;
+  };
+
+  const rollDice = (diceMax) => getRandomByMinMax(1, diceMax);
+
+  const rollD4 = () => rollDice(4);
+  const rollD6 = () => rollDice(6);
+  const rollD8 = () => rollDice(8);
+  const rollD10 = () => rollDice(10);
+  const rollD12 = () => rollDice(12);
+  const rollD20 = () => rollDice(20);
+
+  let value = rollD20();
+
+  const onClick = () => {
+    value = rollD20();
+  };
 </script>
 
 <main>
-  <h1 class="text-3xl font-bold underline">Hello Typescript!</h1>
+  <h1 class="text-3xl font-bold underline">Hello Typescript! {value}</h1>
+  <button on:click={onClick}>Roll!</button>
 </main>
 
 <style>
