277db2385ed0e8ad9ad2ccef2f6a3303fec824d0
Denis Wells <deniswells59@gmail.com>
2022-12-15 11:59:02 -0800

    roll list clear functionality

--- src/lib/RollList.svelte
+++ src/lib/RollList.svelte
@@ -1,19 +1,44 @@
-<script>
+<script lang="ts">
   import { rolls } from '../store';
+  import type { Roll } from '../types/Roll';
 
-  const watch = rolls.subscribe((rolls) => {
-    console.log('rolls:', rolls);
-  });
+  const ROLL_LIST_TIMEOUT = 100000;
+  let showRollList = false;
+  let rollListTimeoutID;
+
+  const handleRollListUpdate = (newRolls: Roll[]) => {
+    if (newRolls.length) {
+      showRollList = true;
+
+      if (rollListTimeoutID) {
+        clearTimeout(rollListTimeoutID);
+      }
+
+      rollListTimeoutID = setTimeout(closeRollList, ROLL_LIST_TIMEOUT);
+    }
+  };
+
+  const closeRollList = () => {
+    showRollList = false;
+    rolls.set([]);
+  };
+
+  rolls.subscribe(handleRollListUpdate);
 </script>
 
-{#if $rolls.length}
-  <ul class="absolute bottom-10 right-10">
-    {#each $rolls.slice(-3) as roll}
-      <li>
-        <div class="flex justify-center items-center shadow-lg my-4 w-80 h-16 border">
-          <p>{roll.value}</p>
-        </div>
-      </li>
-    {/each}
-  </ul>
+{#if showRollList}
+  <div class="absolute bottom-10 right-10 flex flex-col items-end">
+    <ul class="flex flex-col items-end">
+      {#each $rolls.slice(-3) as roll}
+        <li
+          class="flex justify-center items-center shadow-lg my-4 border last:w-80 last:h-16 w-40 h-8"
+        >
+          <div>
+            <p>{roll.value}</p>
+          </div>
+        </li>
+      {/each}
+    </ul>
+    <button on:click={() => closeRollList()}>Clear X</button>
+  </div>
 {/if}
