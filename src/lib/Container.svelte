72e80cdb51151c0a274e5a5b0a5691fdffe040e6
Denis Wells <deniswells59@gmail.com>
2023-01-19 14:36:52 -0800

    container component tests

--- src/lib/Container.svelte
+++ src/lib/Container.svelte
@@ -1,7 +1,12 @@
 <script>
   export let cssClass = '';
+  export let testId = '';
 </script>
 
-<div class={`flex content-center justify-center flex-wrap px-[10px] ${cssClass}`} {...$$restProps}>
+<div
+  class={`flex content-center justify-center flex-wrap px-[10px] ${cssClass}`}
+  {...$$restProps}
+  data-testid={testId}
+>
   <slot />
 </div>
---
+++ src/lib/__tests__/Container.spec.ts
@@ -0,0 +1,21 @@
+import { render } from '@testing-library/svelte';
+
+import Container from '../Container.svelte';
+
+describe('Container Component', () => {
+  test('it extends the container components CSS class', () => {
+    const extendedCSSContainer = render(Container, { testId: 'extendedClass', class: 'bg-yellow' });
+    const defaultCSSContainer = render(Container, { testId: 'defaultClass' });
+
+    const extendedCSSContainerEl = extendedCSSContainer.getByTestId('extendedClass', {
+      exact: true,
+    });
+    const defaultCSSContainerEl = defaultCSSContainer.getByTestId('defaultClass', { exact: true });
+
+    const extendedCSSClass = extendedCSSContainerEl.getAttribute('class');
+    const defaultCSSClass = defaultCSSContainerEl.getAttribute('class');
+
+    expect(extendedCSSClass).toContain('bg-yellow');
+    expect(defaultCSSClass).not.toContain('bg-yellow');
+  });
+});
