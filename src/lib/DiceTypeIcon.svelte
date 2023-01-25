158519539904cb9f30afdd77b2e4ce4eb0806cb4
Denis Wells <deniswells59@gmail.com>
2023-01-25 13:10:35 -0800

    add icons to dice buttons

---
+++ src/lib/DiceTypeIcon.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import { DICE_ASSETS_BY_TYPE } from '../constants/assets';
+
+  export let typeOfDice;
+  export let whiteFill = false;
+
+  console.log('@@typeOfDice:', typeOfDice);
+  const { asset, styleOverride } = DICE_ASSETS_BY_TYPE[typeOfDice];
+</script>
+
+<span
+  class={`inline-block h-[36px] w-[32px] relative mb-[-12px]`}
+  style={`background: ${
+    whiteFill ? asset.whiteFill : asset.blackFill
+  } no-repeat; ${styleOverride}`}
+/>
