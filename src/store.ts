39650993d9bf7ceb98bfe40ae53fe331bedf06ee
Denis Wells <deniswells59@gmail.com>
2023-01-24 19:49:28 -0800

    persist customRoll store with localstorage

--- src/store.ts
+++ src/store.ts
@@ -3,4 +3,17 @@ import type { RollResult, Roll } from './types/Roll';
 
 export const rollResults: Writable<RollResult[]> = writable([]);
 
-export const customRolls: Writable<Roll[]> = writable([]);
+const CUSTOM_ROLLS_STORAGE_NAME = 'custom_rolls';
+const storedCustomRolls = localStorage.getItem(CUSTOM_ROLLS_STORAGE_NAME);
+const parsedStoredCustomRolls = JSON.parse(storedCustomRolls);
+
+export const customRolls: Writable<Roll[]> = writable(
+  parsedStoredCustomRolls || []
+);
+
+customRolls.subscribe((newCustomRolls) => {
+  localStorage.setItem(
+    CUSTOM_ROLLS_STORAGE_NAME,
+    JSON.stringify(newCustomRolls)
+  );
+});
