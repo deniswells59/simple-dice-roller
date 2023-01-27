674a6061eddddd09c265c48c010d32833c05894f
Denis Wells <deniswells59@gmail.com>
2023-01-27 13:58:17 -0800

    website header

--- src/App.svelte
+++ src/App.svelte
@@ -1,15 +1,17 @@
 <script lang="ts">
   import './app.css';
 
+  import Page from './lib/Page.svelte';
+  import SiteHeader from './lib/SiteHeader.svelte';
+  import QuickRollForm from './lib/QuickRollForm.svelte';
   import BaseDiceButtons from './lib/BaseDiceButtons.svelte';
   import CustomDiceButtons from './lib/CustomDiceButtons.svelte';
-  import Page from './lib/Page.svelte';
   import RollResultList from './lib/RollResultList.svelte';
-  import QuickRollForm from './lib/QuickRollForm.svelte';
   import CustomFormModal from './lib/CustomFormModal.svelte';
 </script>
 
 <Page>
+  <SiteHeader />
   <QuickRollForm />
   <BaseDiceButtons />
   <CustomDiceButtons />
