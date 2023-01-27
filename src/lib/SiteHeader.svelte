fb17976446a6149abb6c11df5d4e5ff178f1f039
Denis Wells <deniswells59@gmail.com>
2023-01-27 14:02:46 -0800

    Create Website header (#7)

---
+++ src/lib/SiteHeader.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import Container from './Container.svelte';
+</script>
+
+<Container class="flex flex-col justify-center items-center">
+  <h1 class="text-3xl text-center underline underline-offset-[4px] my-4 leading-10">
+    A Simple RPG Dice Roller
+  </h1>
+  <p class="font-light text-center">
+    Use <span class="font-bold">Quick Roll</span> to roll some dice.
+  </p>
+  <p class="font-light text-center">
+    Use <span class="font-bold">Base Dice</span> to roll one die.
+  </p>
+  <p class="font-light text-center">
+    Or <span class="font-bold">create Custom</span> rolls to save a roll you keep
+    coming back to.
+  </p>
+</Container>
